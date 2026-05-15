3f7f4dd49e3e4910ce00c020dc20ce397dd946db
Rahmatadianto07 <149448744+Rahmatadianto07@users.noreply.github.com>
2026-05-16 01:20:22 +0800

    Update transaksi.ts

--- types/transaksi.ts
+++ types/transaksi.ts
@@ -1,12 +1,21 @@
 export interface Transaksi {
   id: number;
+
   tanggal: string;
+
   saldoAwal: number;
+
   belanja: number;
+
   cashbon: number;
+
   lainnya: number;
+
   markup: number;
+
   saldoAkhir: number;
+
   cash: number;
- 
+
+  keterangan: string;
 }
