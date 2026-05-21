61b462ac9dca5f09a153fa3fd3d16b48df8a5e8c
Rahmatadianto07 <149448744+Rahmatadianto07@users.noreply.github.com>
2026-05-21 10:01:34 +0800

    Update RekapTable.tsx

--- app/components/RekapTable.tsx
+++ app/components/RekapTable.tsx
@@ -406,7 +406,7 @@ export default function RekapTable({
 
                   </p>
 
-                   {/* <button
+                  {/* <button
                     onClick={() =>
                       onEditCatatan(
                         item
